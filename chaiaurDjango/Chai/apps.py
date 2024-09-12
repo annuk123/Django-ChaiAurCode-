567e1beaad347e0bbcd7718aef9852ade9b7084c
ANNU KUMARI <anuk35168@gmail.com>
2024-09-12 20:38:03 +0530

    commit changes

--- chaiaurDjango/Chai/apps.py
+++ chaiaurDjango/Chai/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ChaiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Chai'
+    name = 'chai'
--- chaiaurDjango/chaiaurDjango/urls.py
+++ chaiaurDjango/chaiaurDjango/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('', views.Home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
-    path('Chai/', include('Chai.urls') ),
+    path('chai/', include('chai.urls') ),
 
 ]
